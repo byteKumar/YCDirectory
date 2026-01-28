1dc00783fac4d7359e7da322bf9652f3677cc137
bytekumar <chamansuper1@gmail.com>
2026-01-28 18:04:03 -0500

    Dynamic Routing

--- app/dashboard/users/[id]/page.tsx
+++ app/dashboard/users/[id]/page.tsx
@@ -1,7 +1,5 @@
-import React from "react";
-
-const Page = ({ params }: { params: { id: string }}) => {
-    const { id } = params;
+const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
+    const { id } = await params;
 
     return (
         <h1 className="text-3xl"> User Profile: {id}</h1>
--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import Link from "next/link";
 
-function Users() {
+const Page = () => {
     return (
-        <div>Users Details Page</div>
-    );
+        <div>
+            <div>Dashboard Users</div>
+
+            <ul className="mt-10">
+                <li><Link href="/dashboard/users/1">User 1</Link></li>
+                <li><Link href="/dashboard/users/2">User 2</Link></li>
+                <li><Link href="/dashboard/users/3">User 3</Link></li>
+                <li><Link href="/dashboard/users/4">User 4</Link></li>
+            </ul>
+        </div>
+    )
 }
 
-export default Users;
+export default Page;
